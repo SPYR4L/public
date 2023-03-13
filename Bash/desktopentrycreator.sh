ffdfde51130e58fe59e223bf5ee02dde9543a40c
SPYR4L <83414519+SPYR4L@users.noreply.github.com>
2023-03-13 02:47:35 +0100

    Create desktopentrycreator.sh

---
+++ Bash/desktopentrycreator.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+echo "Desktop entry creator - Requires sudo!"
+read -p "Desktop entry name: " desktopentryname
+read -p "Version: " version
+read -p "Application name: " appname
+read -p "Comment which can be used as a tooltip: " appcomment
+read -p "Path to the folder in which the executable is run: " folderpath
+read -p "The executable file: " executable
+read -p "Icon that will be used to display this entry: " iconpath
+read -p "Needs terminal(True/False): " needsterminal
+
+echo "[Desktop Entry]
+
+# The type as listed above
+Type=Application
+
+# The version of the desktop entry specification to which this file complies
+Version=$version
+
+# The name of the application
+Name=$appname
+
+# A comment which can/will be used as a tooltip
+Comment=$appcomment
+
+# The path to the folder in which the executable is run
+Path=$folderpath
+
+# The executable of the application, possibly with arguments.
+Exec=$executable
+
+# The name of the icon that will be used to display this entry
+Icon=$iconpath
+
+# Describes whether this application needs to be run in a terminal or not
+Terminal=$needsterminal" >> $desktopentryname.desktop && mv $desktopentryname.desktop /usr/share/applications
